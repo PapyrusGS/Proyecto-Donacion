5a6c781fdd642a57e1736352325f3de975324dd2
PapyrusGS <156978051+PapyrusGS@users.noreply.github.com>
2025-08-16 21:17:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,24 +20,7 @@ conexion.connect ((err) => {
 })
 
 app.get('/Donaciones', (req,res) => {
-    const sql = `SELECT 
-        d.id_donacion,
-        CONCAT(do.nombre, ' ', do.apellido) AS donante,
-        do.ci,
-        do.email,
-        d.tipo_donacion,
-        COALESCE(o.nombre, ts.tipo) AS detalle_donado,
-        COALESCE(dd.cantidad, 1) AS cantidad,
-        h.nombre AS hospital,
-        d.fecha_donacion,
-        d.estado AS estado_donacion
-            FROM Donaciones d
-            INNER JOIN Donantes do ON d.id_donante = do.id_donante
-            INNER JOIN Hospitales h ON d.id_hospital = h.id_hospital
-            LEFT JOIN DetalleDonacion dd ON d.id_donacion = dd.id_donacion
-            LEFT JOIN Organos o ON dd.id_organo = o.id_organo
-            LEFT JOIN TiposSangre ts ON dd.id_tipo_sangre = ts.id_tipo_sangre
-            ORDER BY d.fecha_donacion;`;
+    const sql = `Select * from donantes`;
     conexion.query(sql,(err,result) =>{
         if (err) {
             console.log('Error');
