47461a9f5fec7f629866b5659be5e354f51400fb
hugolimari <hugomdaza@gmail.com>
2025-08-16 21:20:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mysql = require('mysql2');
 const app = express();
 const puerto = 3000;
-app.use(express.json());
 
 const conexion = mysql.createConnection({
     host: 'localhost',
